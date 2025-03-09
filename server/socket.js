f004be877f824182e474265def825a7f980835fe
kaushiktak19 <kaushiktak19@gmail.com>
2025-03-10 01:06:12 +0530

    added docker in backend for ec2 hosting & cors url update

--- server/socket.js
+++ server/socket.js
@@ -8,7 +8,7 @@ let io = null;
 function initializeSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: 'https://link-drop.vercel.app',
       methods: ["GET", "POST"],
       credentials: true,
     },
